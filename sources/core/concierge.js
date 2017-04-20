fc7b15e466864e2882c31f6398a3bc806b049114
Maël Nison <nison.mael@gmail.com>
2017-04-20 23:00:36 +0100

    Fixes package on OSX

--- sources/core/concierge.js
+++
@@ -1,3 +0,0 @@
-import { Concierge } from './Concierge';
-
-export let concierge = new Concierge();
--- sources/core/index.js
+++ sources/core/index.js
@@ -1,4 +1,8 @@
-export * from './concierge';
+export { UsageError } from './UsageError';
+
+import { Concierge } from './Concierge';
+export { Concierge } from './Concierge';
+export let concierge = new Concierge();
 
 import * as flags from './flags';
 export { flags };
