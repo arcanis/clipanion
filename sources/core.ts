ac86d5c8839a32d843f318b80c24a8827118a208
Maël Nison <nison.mael@gmail.com>
2020-01-13 19:16:29 +0100

    Fixes carriage returns in bindings

--- sources/core.ts
+++ sources/core.ts
@@ -15,7 +15,7 @@ export const HELP_COMMAND_INDEX = -1;
 export const HELP_REGEX = /^(-h|--help)(?:=([0-9]+))?$/;
 export const OPTION_REGEX = /^(--[a-z]+(?:-[a-z]+)*|-[a-zA-Z]+)$/;
 export const BATCH_REGEX = /^-[a-zA-Z]{2,}$/;
-export const BINDING_REGEX = /^([^=]+)=(.*)$/;
+export const BINDING_REGEX = /^([^=]+)=([\s\S]*)$/;
 
 // ------------------------------------------------------------------------
 
