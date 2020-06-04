6589095446f631ac980a6981806d601d02d8c3bb
merceyz <merceyz@users.noreply.github.com>
2020-06-04 18:46:11 +0200

    build: bundle using rollup

---
+++ rollup.config.js
@@ -0,0 +1,18 @@
+import ts from "@wessberg/rollup-plugin-ts";
+
+export default {
+  input: './sources/advanced/index.ts',
+  output: [
+    {
+      file: 'lib/index.mjs',
+      format: 'es'
+    },
+    {
+      file: 'lib/index.js',
+      format: 'cjs'
+    },
+  ],
+  plugins: [
+    ts(),
+  ]
+}
