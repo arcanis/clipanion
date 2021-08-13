f72049a0f99c947a852c2c188fa98cbf94b9715f
Paul Soporan <paul.soporan@gmail.com>
2021-08-13 17:52:19 +0300

    fix: redirect stderr to $null in powershell driver

--- clcs/sources/drivers/PowerShellDriver.ts
+++ clcs/sources/drivers/PowerShellDriver.ts
@@ -39,7 +39,7 @@ const PowerShellDriver: ShellDriver = {
   getCompletionProvider: ({binaryName, requestCompletionCommand}) => `
     Register-ArgumentCompleter -Native -CommandName ${binaryName} -ScriptBlock {
       param($wordToComplete, $commandAst, $cursorPosition)
-        $results = ${requestCompletionCommand} ${PowerShellDriver.shellName} "--" "$commandAst" "$cursorPosition" | ConvertFrom-Json
+        $results = ${requestCompletionCommand} ${PowerShellDriver.shellName} "--" "$commandAst" "$cursorPosition" 2>$null | ConvertFrom-Json
         $results |
           # select completions that match the original word
           ? { $_.CompletionText -like "$wordToComplete*" } |
