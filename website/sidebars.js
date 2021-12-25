6422b808ede2b4b9c525a4a93933b013be156cab
Maël Nison <nison.mael@gmail.com>
2021-12-25 18:53:22 +0100

    Updates website

--- website/sidebars.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  someSidebar: {
-    General: [`overview`, `getting-started`, `paths`, `options`, `contexts`, `validation`, `help`, `tips`],
-    API: [`api/cli`, `api/builtins`, `api/option`],
-  },
-};
