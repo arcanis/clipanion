cbaf57f0f5723f48deb76df0741bd77e2db8de5e
Maël Nison <nison.mael@gmail.com>
2022-12-14 12:27:40 +0100

    Update config.js

--- website/config.js
+++ website/config.js
@@ -15,7 +15,7 @@ module.exports = {
   },
 
   sidebar: {
-    General: [`overview`, `getting-started`, `paths`, `options`, `contexts`, `validation`, `help`, `tips`],
+    General: [`overview`, `getting-started`, `paths`, `options`, `contexts`, `validation`, `errors`, `help`, `tips`],
     API: [`api/cli`, `api/builtins`, `api/option`],
   },
 
